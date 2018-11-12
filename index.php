ea01c703f18cd0543bdd3916c1ed35454f683fb9
sarah <>
2018-11-12 23:04:26 +0100

    afleverer

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Index</title>
+</head>
+
+<body>
+    <?php include "header.html";?>
+    <?php include "index.html";?>
+
+    <main>
+
+
+    </main>
+    <?php include "footer.html"; ?>
+</body>
+</html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
