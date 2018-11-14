025d8b33a6d27c60d5941aceaef9096545a04c65
mskovn <metteskovn@gmail.com>
2018-11-14 13:15:25 +0100

    flere småting

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+     <link rel="icon" href="Icon-16.png" type="image" sizes="16x16">
+        <link rel="apple-touch-icon" sizes="180x180" href="Icon-180.png">
     <title>Huset KBH</title>
 </head>
 
